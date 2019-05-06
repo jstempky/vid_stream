5028f3877af2c6f97419deef2ca2f7bbe338dc88
Joey Stempky <joeystempky@Joeys-Air.attlocal.net>
2019-05-06 06:40:00 -0400

    Monday update

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
@@ -11,9 +12,7 @@ class StreamList extends React.Component {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">
-            Edit
-          </button>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
           <button className="ui button negative">
             Delete
           </button>
@@ -26,15 +25,27 @@ class StreamList extends React.Component {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
-          {this.renderAdmin(stream)}
         </div>
       );
     });
+  }
+
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div>
+          <Link to="/streams/new" className="ui button primary">
+            CreateStream
+          </Link>
+        </div>
+      );
+    }
   }
 
   render() {
@@ -42,6 +53,7 @@ class StreamList extends React.Component {
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
   }
@@ -50,7 +62,8 @@ class StreamList extends React.Component {
 const mapStateToProps = (state) => {
   return {
     streams: Object.values(state.streams),
-    currentUserId: state.auth.userId
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
   };
 };
 
